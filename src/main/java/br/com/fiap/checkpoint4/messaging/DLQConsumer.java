487b0fe7679a3c4f90ef59350e6f827e71109c9f
devpedrosena1 <devpedrosena1@gmail.com>
2026-04-21 12:23:25 -0300

    changing queue name DQL -> DLQ

--- src/main/java/br/com/fiap/checkpoint4/messaging/DLQConsumer.java
+++ src/main/java/br/com/fiap/checkpoint4/messaging/DLQConsumer.java
@@ -10,12 +10,12 @@ public class DLQConsumer {
 
     @JmsListener(destination = "ActiveMQ.DLQ", containerFactory = "jmsListenerContainerFactory")
     public void onDqlMessage(Event event) {
-        System.out.println("DQL recebeu: " + event.payload());
+        System.out.println("DLQ recebeu: " + event.payload());
     }
 
     @JmsListener(destination = "order.receiver.CustomDLQ", containerFactory = "jmsListenerContainerFactory")
     public void onCustomDqlMessage(Event event) {
-        System.out.println("Custom DQL recebeu: " + event.payload());
+        System.out.println("Custom DLQ recebeu: " + event.payload());
     }
 
 }
